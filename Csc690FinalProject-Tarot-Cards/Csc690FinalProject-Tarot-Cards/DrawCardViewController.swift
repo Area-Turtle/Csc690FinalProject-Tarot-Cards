87a0cc2834dd0404bb6ae98e098e4715714d3d28
Area-Turtle <60633052+Area-Turtle@users.noreply.github.com>
2021-05-13 04:32:14 -0700

    added desc

--- Csc690FinalProject-Tarot-Cards/Csc690FinalProject-Tarot-Cards/DrawCardViewController.swift
+++ Csc690FinalProject-Tarot-Cards/Csc690FinalProject-Tarot-Cards/DrawCardViewController.swift
@@ -14,6 +14,8 @@ class DrawCardViewController: UIViewController {
     @IBOutlet weak var present: UIImageView!
     @IBOutlet weak var past: UIImageView!
     @IBOutlet weak var future: UIImageView!
+    @IBOutlet weak var message: UILabel!
+    
     var values = [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     var suits = ["cup", "pentacle", "sword", "wand"]
     
@@ -22,19 +24,43 @@ class DrawCardViewController: UIViewController {
     
     var desc = [
         /* cups: [2,3,4,5,6,7,8,9,10,11,12,13,14,15] */
-        "unity", "friendship", "apathy", "loss", "familiarity", "searching", "walking away", "satisfaction", "inner happiness", "happy surpise", "following the heard", "compassion", "compassion", "new feelings",
+        ["cup2","unity"],["cup3", "friendship"],
+        ["cup4", "apathy"],["cup5", "loss"],
+        ["cup6", "familiarity"],["cup7", "searching"],
+        ["cup8", "walking away"],["cup9", "satisfaction"],
+        ["cup10", "inner happiness"],["cup11", "happy surpise"],
+        ["cup12", "following the heard"],["cup13", "compassion"],
+        ["cup14", "compassion"],["cup15", "new feelings"],
         
         /*pentacles [2,3,4,5,6,7,8,9,10,11,12,13,14,15]*/
-        "balancing", "teamwork", "conversation", "need", "charity", "hardwork", "apprenticeship", "fruits of labor", "legacy", "abition", "efficiency", "practicality", "abundance", "opportunity",
+        ["pentacle2","balancing"],["pentacle3", "teamwork"],
+        ["pentacle4", "conversation"],["pentacle5", "need"],
+        ["pentacle6", "charity"],["pentacle7", "hardwork"],
+        ["pentacle8","apprenticeship"],["pentacle9", "fruits of labor"],
+        ["pentacle10","legacy"],["pentacle11", "abition"],
+        ["pentacle12", "efficiency"],["pentacle13", "practicality"],
+        ["pentacle14", "abundance"],["pentacle15", "opportunity"],
         /*swords [2,3,4,5,6,7,8,9,10,11,12,13,14,15]*/
-        "difficult choices", "rest", "heartbreak", "unbridled ambition", "transition", "deception", "imprisionment", "anxiety", "failure", "curiosity", "complexity", "action", "head over heart", "breakthrough",
+        ["sword2", "difficult choices"],["sword3", "rest"],
+        ["sword4", "heartbreak"],["sword5", "unbridled ambition"],
+        ["sword6", "transition"],["sword7", "deception"],
+        ["sword8", "imprisionment"],["sword9", "anxiety"],
+        ["sword10", "failure"],["sword11", "curiosity"],
+        ["sword12", "complexity"],["swor13", "action"],
+        ["sword14", "head over heart"],["sword15", "breakthrough"],
         /*wands [2,3,4,5,6,7,8,9,10,11,12,13,14,15]*/
-        "planning", "looking ahead", "community", "competition", "victory", "perseverance", "rapid action", "resilience", "accomplishment", "exploration", "action", "courage", "big picture", "creation"]
+        ["wand2", "planning"],["wand3", "looking ahead"],
+        ["wand4", "community"],["wand5", "competition"],
+        ["wand6", "victory"],["wand7", "perseverance"],
+        ["wand8", "rapid action"],["wand9", "resilience"],
+        ["wand10", "accomplishment"],["wand11", "exploration"],
+        ["wand12", "action"],["wand13", "courage"],
+        ["wand14","big picture"],["wand15", "creation"]]
+    
     var desc2 = [
         
-        
         /*majors: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]*/
-        "innocence", "willpwer", "intuitive", "motherhood", "authority", "tradition", "partnerships", "direction", "inner strengh", "contemplation", "change", "cause/effect", "sacrifice", "end of cycle", "middle path", "addiction", "sudden upheaval", "hope","unconscious", "joy", "reflection", "fulfilment" ]
+        ["major0", "innocence"],["major1", "willpwer"],["major2", "intuitive"],["major3", "motherhood"],["major4", "authority"],["major5", "tradition"],["major6", "partnerships"],["major7","direction"],["major8", "inner strengh"],["major9", "contemplation"],["major10", "change"],["major11", "cause/effect"],["major12", "sacrifice"],["major13", "end of cycle"],["major14", "middle path"],["major15", "addiction"],["major16", "sudden upheaval"],["major17", "hope"],["major18","unconscious"],["major19", "joy"],["major20", "reflection"],["major21", "fulfilment"] ]
     
     var minorArcana: Deck!
     var majorArcana: Deck!
@@ -58,6 +84,11 @@ class DrawCardViewController: UIViewController {
         for k in 0...21{
             Tarot.append(majorArcana.tarot[k])
             //print(majorArcana.tarot[k])
+        }
+        for i in 0...21{
+            for j in 0..<1{
+                print(desc2[i][0])
+            }
         }
         /*
          for j in 0...77{
@@ -85,6 +116,10 @@ class DrawCardViewController: UIViewController {
         present.image = UIImage(named: "\(player[0].toString())")
         past.image = UIImage(named: "\(player[1].toString())")
         future.image = UIImage(named: "\(player[2].toString())")
+        
+        
+        
+        //message.text = "
     }
     
     
